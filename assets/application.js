b683cf453123e6eddb1200efc647ae11c3c343d2
Square-Logic <markustryban@squarelogic.digital>
2021-06-04 16:01:33 -0400

    Refactor application.js for currency change

--- assets/application.js
+++ assets/application.js
@@ -1,50 +1,67 @@
-  /* Fix for customer account pages */
-  jQuery('span.money span.money').each(function() {
-    jQuery(this).parents('span.money').removeClass('money');
-  });
+$(document).ready(function () {
+  let
+    moneySpanSelector = 'span.money',
+    currencyPickerSelector = '[name=currencies]';
 
-  /* Saving the current price */
-  jQuery('span.money').each(function() {
-    jQuery(this).attr('data-currency-{{ shop.currency }}', jQuery(this).html());
-  });
+  let currencyPicker = {
+    loadCurrency: function () {
+      /* Fix for customer account pages */
+      $(moneySpanSelector + ' ' + moneySpanSelector).each(function () {
+        $(this).parents(moneySpanSelector).removeClass('money');
+      });
 
-  // If there's no cookie.
-  if (cookieCurrency == null) {
-    if (shopCurrency !== defaultCurrency) {
-      Currency.convertAll(shopCurrency, defaultCurrency);
+      /* Saving the current price */
+      $(moneySpanSelector).each(function () {
+        $(this).attr('data-currency-'+shopCurrency, $(this).html());
+      });
+
+      // If there's no cookie.
+      if (cookieCurrency == null) {
+        if (shopCurrency !== defaultCurrency) {
+          Currency.convertAll(shopCurrency, defaultCurrency);
+        }
+        else {
+          Currency.currentCurrency = defaultCurrency;
+        }
+      }
+      // If the cookie value does not correspond to any value in the currency dropdown.
+      else if ($(currencyPickerSelector).length && $(currencyPickerSelector + 'option[value=' + cookieCurrency + ']').length === 0) {
+        Currency.currentCurrency = shopCurrency;
+        Currency.cookie.write(shopCurrency);
+      }
+      else if (cookieCurrency === shopCurrency) {
+        Currency.currentCurrency = shopCurrency;
+      }
+      else {
+        Currency.convertAll(shopCurrency, cookieCurrency);
+      }
+    },
+    onCurrencyChanged: function (event) {
+      let
+        newCurrency = $(this).val(),
+        otherPickers = $(currencyPickerSelector).not($(this));
+
+      Currency.convertAll(Currency.currentCurrency, newCurrency);
+
+      if (otherPickers.length > 0) {
+        otherPickers.val(newCurrency);
+      }
+    },
+    onMoneySpanAdded: function () {
+      Currency.convertAll(shopCurrency, Currency.currentCurrency);
+    },
+    init: function () {
+      if (showMultipleCurrencies !== true) {
+        return false;
+      }
+
+      currencyPicker.loadCurrency();
+
+      $(document).on('change', currencyPickerSelector, currencyPicker.onCurrencyChanged);
     }
-    else {
-      Currency.currentCurrency = defaultCurrency;
-    }
-  }
-  // If the cookie value does not correspond to any value in the currency dropdown.
-  else if (jQuery('[name=currencies]').length && jQuery('[name=currencies] option[value=' + cookieCurrency + ']').length === 0) {
-    Currency.currentCurrency = shopCurrency;
-    Currency.cookie.write(shopCurrency);
-  }
-  else if (cookieCurrency === shopCurrency) {
-    Currency.currentCurrency = shopCurrency;
-  }
-  else {
-    Currency.convertAll(shopCurrency, cookieCurrency);
-  }
-
-  jQuery('[name=currencies]').val(Currency.currentCurrency).change(function() {
-    var newCurrency = jQuery(this).val();
-    Currency.convertAll(Currency.currentCurrency, newCurrency);
-    jQuery('.selected-currency').text(Currency.currentCurrency);
-  });
-
-  var original_selectCallback = window.selectCallback;
-  var selectCallback = function(variant, selector) {
-    original_selectCallback(variant, selector);
-    Currency.convertAll(shopCurrency, jQuery('[name=currencies]').val());
-    jQuery('.selected-currency').text(Currency.currentCurrency);
   };
 
-  $('body').on('ajaxCart.afterCartLoad', function(cart) {
-    Currency.convertAll(shopCurrency, jQuery('[name=currencies]').val());
-    jQuery('.selected-currency').text(Currency.currentCurrency);  
-  });
+  currencyPicker.init();
 
-  jQuery('.selected-currency').text(Currency.currentCurrency);
+});
+
