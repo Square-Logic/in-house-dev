4342aaf48aa5d2f3d7c5a85ff8a856e58fe1e4bf
Square-Logic <markustryban@squarelogic.digital>
2021-06-08 13:08:51 -0400

    Add line item quantity picker & quantity fields

--- assets/application.js
+++ assets/application.js
@@ -247,5 +247,57 @@ $(document).ready(function() {
 
   ajaxify.init();
 
+  // Quantity Fields
+  let
+    quantityFieldSelector = '.js-quantity-field',
+    quantityButtonSelector = '.js-quantity-button',
+    quantityPickerSelector = '.js-quantity-picker',
+    quantityPicker = {
+      onButtonClick: function(event) {
+        let
+          button = $(this),
+          picker = button.closest(quantityPickerSelector),
+          quantity = picker.find('.js-quantity-field'),
+          quantityValue = parseInt(quantity.val()),
+          max = quantity.attr('max') ? parseInt(quantity.attr('max')) : null;
+
+        if (button.hasClass('plus') && (max === null || quantityValue+1 <= max)) {
+          quantity.val(quantityValue + 1).change();
+        } else if (button.hasClass('minus')) {
+          quantity.val(quantityValue - 1).change();
+        }
+      },
+      onChange: function(event) {
+        let
+          field = $(this),
+          picker = field.closest(quantityPickerSelector),
+          quantityText = picker.find('.js-quantity-text'),
+          shouldDisableMinus = parseInt(this.value) === parseInt(field.attr('min')),
+          shouldDisablePlus = parseInt(this.value) === parseInt(field.attr('max')),
+          minusButton = picker.find('.js-quantity-button.minus'),
+          plusButton = picker.find('.js-quantity-button.plus');
+
+        quantityText.text(this.value);
+        
+        if (shouldDisableMinus) {
+          minusButton.prop('disabled', true);
+        } else if (minusButton.prop('disabled') === true) {
+          minusButton.prop('disabled', false)
+        }
+
+        if (shouldDisablePlus) {
+          plusButton.prop('disabled', true);
+        } else if (plusButton.prop('disabled') === true) {
+          plusButton.prop('disabled', false);
+        }
+      },
+      init: function() {
+        $(document).on('click', quantityButtonSelector, quantityPicker.onButtonClick);
+        $(document).on('change', quantityFieldSelector, quantityPicker.onChange);
+      }
+    };
+
+    quantityPicker.init();
+
 });
 
